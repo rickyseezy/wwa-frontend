c30ed49f5e19ccd2ec8682a6bae3209a406b6f67
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-02-24 09:37:33 +0100

    europe link

--- components/continent-menu/item/Item.tsx
+++ components/continent-menu/item/Item.tsx
@@ -4,189 +4,147 @@ import { MouseEventHandler, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 
 interface IContinentMenuItemProps {
-    continent: string;
-
+  continent: string;
 }
 
 interface IContinentConfig {
-    title: string;
-    imgSrc: string;
-    custom: string;
+  title: string;
+  imgSrc: string;
+  custom: string;
 }
 
-// ToDo :: Keep adding continents
-// const continentConfig = new Map<string, IContinentConfig>([
-//     [
-//         "EUROPE", {
-//             title: "EUROPE",
-//             imgSrc: "/images/europe-item.png",
-//             custom: `${styles["item--europe"]}`
-//         }
-//     ],
-//     [
-//         "AFRICA", {
-//             title: "AFRICA",
-//             imgSrc: "/images/africa-item.png",
-//             custom: `${styles["item--africa"]}`
-//         }
-//     ],
-//     [
-//         "NORTH-US", {
-//             title: "AMÉRIQUE DU NORD",
-//             imgSrc: "/images/north-us-item.png",
-//             custom: `${styles["item--northus"]}`
-//         }
-//     ],
-//     [
-//         "SOUTH-AMERICA", {
-//             title: "AMÉRIQUE DU SUD",
-//             imgSrc: "/images/south-america-item.png",
-//             custom: `${styles["item--southamerica"]}`
-//         }
-//     ],
-//     [
-//         "ASIA", {
-//             title: "ASIE",
-//             imgSrc: "/images/asia-item.png",
-//             custom: `${styles["item--asia"]}`
-//         }
-//     ],
-//     [
-//         "OCEANIA", {
-//             title: "OCÈANIE",
-//             imgSrc: "/images/oceania-item.png",
-//             custom: `${styles["item--oceania"]}`
-//         }
-//     ]
-// ]);
 const continentConfig = new Map<string, IContinentConfig>([
-    [
-        "EUROPE", {
-            title: "EUROPE",
-            imgSrc: "/images/europe-item.png",
-            custom: `${styles["item--europe"]}`
-        }
-    ],
-    [
-        "AFRICA", {
-            title: "AFRICA",
-            imgSrc: "/images/africa-item.png",
-            custom: `${styles["item--africa"]}`
-        }
-    ],
-    [
-        "NORTH-US", {
-            title: "AMÉRIQUE DU NORD",
-            imgSrc: "/images/north-us-item.png",
-            custom: `${styles["item--northus"]}`
-        }
-    ],
-    [
-        "SOUTH-AMERICA", {
-            title: "AMÉRIQUE DU SUD",
-            imgSrc: "/images/south-america-item.png",
-            custom: `${styles["item--southamerica"]}`
-        }
-    ],
-    [
-        "ASIA", {
-            title: "ASIE",
-            imgSrc: "/images/asia-item.png",
-            custom: `${styles["item--asia"]}`
-        }
-    ],
-    [
-        "OCEANIA", {
-            title: "OCÈANIE",
-            imgSrc: "/images/oceania-item.png",
-            custom: `${styles["item--oceania"]}`
-        }
-    ]
+  [
+    "EUROPE",
+    {
+      title: "EUROPE",
+      imgSrc: "/images/europe-item.png",
+      custom: `${styles["item--europe"]}`,
+    },
+  ],
+  [
+    "AFRICA",
+    {
+      title: "AFRICA",
+      imgSrc: "/images/africa-item.png",
+      custom: `${styles["item--africa"]}`,
+    },
+  ],
+  [
+    "NORTH-US",
+    {
+      title: "AMÉRIQUE DU NORD",
+      imgSrc: "/images/north-us-item.png",
+      custom: `${styles["item--northus"]}`,
+    },
+  ],
+  [
+    "SOUTH-AMERICA",
+    {
+      title: "AMÉRIQUE DU SUD",
+      imgSrc: "/images/south-america-item.png",
+      custom: `${styles["item--southamerica"]}`,
+    },
+  ],
+  [
+    "ASIA",
+    {
+      title: "ASIE",
+      imgSrc: "/images/asia-item.png",
+      custom: `${styles["item--asia"]}`,
+    },
+  ],
+  [
+    "OCEANIA",
+    {
+      title: "OCÈANIE",
+      imgSrc: "/images/oceania-item.png",
+      custom: `${styles["item--oceania"]}`,
+    },
+  ],
 ]);
 
-let tabItems = [] 
+let tabItems = [];
 
-const ContinentMenuItem = ({ continent }: IContinentMenuItemProps) => {   
-    
-    let [selectCountry,setSelectCountry] = useState()
-    const router = useRouter()
-    const config = continentConfig.get(continent);
-    const classes: string[] = [styles["item"]];
-    classes.push(config.custom);
-    let divItems = useRef(null)
-function Clean(){
-    // si page est sur cause 
-    if( router.query.index === undefined){
-        tabItems.map((el, i) => {
-
-            if (i != 0) {
-                el.style = `  background-color: white !important;
+const ContinentMenuItem = ({ continent }: IContinentMenuItemProps) => {
+  let [selectCountry, setSelectCountry] = useState();
+  const router = useRouter();
+  const config = continentConfig.get(continent);
+  const classes: string[] = [styles["item"]];
+  classes.push(config.custom);
+  let divItems = useRef(null);
+  function Clean() {
+    // si page est sur cause
+    if (router.query.index === undefined) {
+      tabItems.map((el, i) => {
+        if (i != 0) {
+          el.style = `  background-color: white !important;
         border: none !important;
-        color: black !important;`
-            }
-         })
-    }else{
-        tabItems.map((el, i) => {
-// si la page est sur countries reset le styles des elements qui ne sont pas égale a l'index
-            if (router.query.index != el.children[1].innerText) {
-                el.style = `  background-color: white !important;
-        border: none !important;
-        color: black !important;`
-            }
-         })
-    }
-}
-
-    useEffect(() => {
-        tabItems.push(divItems.current)
-      
-        // reset les classes background white sauf la premiere 
-        tabItems[0].classList.add("item--europe")
-
-      Clean()
-      
-        return () => {
-            tabItems.length = 0
+        color: black !important;`;
         }
-    }, [router.query.index])
+      });
+    } else {
+      tabItems.map((el, i) => {
+        // si la page est sur countries reset le styles des elements qui ne sont pas égale a l'index
+        if (router.query.index != el.children[1].innerText) {
+          el.style = `  background-color: white !important;
+        border: none !important;
+        color: black !important;`;
+        }
+      });
+    }
+  }
 
-    function Select(e) {
-        let tabItemsClasses : Object[]  = Array.from(continentConfig.values())
-        const target = e.target as HTMLDivElement;
+  useEffect(() => {
+    tabItems.push(divItems.current);
 
-    setSelectCountry(e.target)
+    // reset les classes background white sauf la premiere
+    tabItems[0].classList.add("item--europe");
 
-        // reset les classes bacground white
-        tabItemsClasses.forEach((_el, i) => {
-             
+    Clean();
 
-            tabItems[i].style = `  background-color: white !important;
+    return () => {
+      tabItems.length = 0;
+    };
+  }, [router.query.index]);
+
+  function Select(e) {
+    let tabItemsClasses: Object[] = Array.from(continentConfig.values());
+    const target = e.target as HTMLDivElement;
+
+    setSelectCountry(e.target);
+
+    // reset les classes bacground white
+    tabItemsClasses.forEach((_el, i) => {
+      tabItems[i].style = `  background-color: white !important;
                border: none !important;
                 color: black !important;
-               `
-        })
+               `;
+    });
+
+    // met la classe target du background correspondant
+
+    e.target.style = "border: 1px solid transparent";
+    target.classList.add(classes[1]);
 
 
 
+    if(config.title !== 'EUROPE'){
+   return     router.push("/projects/countries/" + config.title);
 
-        // met la classe target du background correspondant 
-
-         e.target.style = 'border: 1px solid transparent'
-        target.classList.add(classes[1])
-        router.push('/projects/countries/'+ config.title)
-       
-
+    }else{
+        return router.push("/projects");
     }
+  }
 
-
-    return (
-        <div className={classes.join(" ")} onClick={Select} ref={divItems}>
-            <div className={styles["item__img"]}>
-                <img src={config.imgSrc} />
-            </div>
-            <div className={styles["item__title"]}>{config.title}</div>
-        </div>
-    );
+  return (
+    <div className={classes.join(" ")} onClick={Select} ref={divItems}>
+      <div className={styles["item__img"]}>
+        <img src={config.imgSrc} />
+      </div>
+      <div className={styles["item__title"]}>{config.title}</div>
+    </div>
+  );
 };
 
 export default ContinentMenuItem;
--- components/menuCountry/MenuCountry.tsx
+++ components/menuCountry/MenuCountry.tsx
@@ -1,144 +1,141 @@
 import { useRouter } from "next/router";
-import { useEffect, useState,useRef, MouseEventHandler } from "react";
+import { useEffect, useState, useRef, MouseEventHandler } from "react";
 import styles from "../layouts/projects/ProjectsLayout.module.scss";
 
-interface MenuCountryProps{
-    select : MouseEventHandler<HTMLLIElement>
+interface MenuCountryProps {
+  select: MouseEventHandler<HTMLLIElement>;
 }
-
 
 const continentConfig = new Map([
   [
-      "EUROPE", {
-          pays: ['Italie','Espagne','Portugal','Allemagne'],
-      }
+    "EUROPE",
+    {
+      pays: ["Italie", "Espagne", "Portugal", "Allemagne"],
+    },
   ],
   [
-      "AFRICA", {
-        pays: ['Sénégale','Mali','Nigéria','Congo'],
-
-      }
+    "AFRICA",
+    {
+      pays: ["Sénégale", "Mali", "Nigéria", "Congo"],
+    },
   ],
   [
-      "AMÉRIQUE DU NORD", {
-        pays: ['New York','Atlanta','Philadelphie','Chicago'],
-
-      }
+    "AMÉRIQUE DU NORD",
+    {
+      pays: ["New York", "Atlanta", "Philadelphie", "Chicago"],
+    },
   ],
   [
-      "AMÉRIQUE DU SUD", {
-        pays: ['Brésil','Colombie','Venezuela','Mexique'],
-
-      }
+    "AMÉRIQUE DU SUD",
+    {
+      pays: ["Brésil", "Colombie", "Venezuela", "Mexique"],
+    },
   ],
   [
-      "ASIE", {
-        pays: ['Japon','Chine','Inde','Thailande'],
-
-      }
+    "ASIE",
+    {
+      pays: ["Japon", "Chine", "Inde", "Thailande"],
+    },
   ],
   [
-      "OCÈANIE", {
-        pays: ['Australie','Fidji','Micronésie','Tonga']
-      }
-  ]
+    "OCÈANIE",
+    {
+      pays: ["Australie", "Fidji", "Micronésie", "Tonga"],
+    },
+  ],
 ]);
 
+function MenuCountry({ select }: MenuCountryProps) {
+  const [isShown, toggleButton] = useState(false);
+  let [tabcountry, settabcountry] = useState([]);
+  let scrollable = useRef();
+  let router = useRouter();
+  let pays = useRef(null);
+  pays.current = [];
 
-function MenuCountry({select}:MenuCountryProps) {
-    const [isShown, toggleButton] = useState(false);
-    let [tabcountry,settabcountry] = useState([])
-    let scrollable = useRef()
-    let router = useRouter()
-    let pays = useRef(null)
-    pays.current = []
+  const scrollButton = (
+    <div className={`${styles["bullet"]}`}>
+      <img
+        className={styles["bullet__arrow"]}
+        src={`/icons/right-arrow-blue.svg`}
+        alt="left arrow"
+      />
+    </div>
+  );
+  function showScrollableMenu(element: HTMLElement) {
+    toggleButton(
+      element.scrollHeight > element.clientHeight ||
+        element.scrollWidth > element.clientWidth
+    );
+  }
 
-    const scrollButton = (
-        <div className={`${styles["bullet"]}`}>
-          <img
-            className={styles["bullet__arrow"]}
-            src={`/icons/right-arrow-blue.svg`}
-            alt="left arrow"
-          />
-        </div>
-      )
-    function showScrollableMenu(element: HTMLElement) {
-        toggleButton(
-          element.scrollHeight > element.clientHeight ||
-          element.scrollWidth > element.clientWidth
-        );
-      }
+  function AddCountry(continent) {
+    let tabPays = continentConfig.get(continent).pays;
+    settabcountry(tabPays);
+  }
 
-      function AddCountry(continent){
-       let tabPays = continentConfig.get(continent).pays;
-       settabcountry(tabPays)
-    
-      } 
+  function MenuSelected(e) {
+    pays.current.map((pay) => {
+      pay.classList.remove("ProjectsLayout_menu__link--selected__Is402");
+    });
+    e.target.classList.add("ProjectsLayout_menu__link--selected__Is402");
+  }
 
-      function MenuSelected(e){
-        pays.current.map(pay =>{
-          pay.classList.remove('ProjectsLayout_menu__link--selected__Is402')
+  useEffect(() => {
+    const scroll = scrollable.current;
 
-        })
-        e.target.classList.add('ProjectsLayout_menu__link--selected__Is402')
-      }
+    showScrollableMenu(scroll);
+    if (router.query.index) {
+      AddCountry(router.query.index);
+      pays.current[0].classList.add(
+        "ProjectsLayout_menu__link--selected__Is402"
+      );
+    } else {
+      AddCountry("EUROPE");
+    }
+  }, [router.query.index]);
 
-      useEffect(()=>{
-        const scroll =  scrollable.current
+  function ChangePays(e) {
+    if (!pays.current.includes(e)) {
+      pays.current.push(e);
+      pays.current = pays.current.filter((e) => e !== null);
+    }
+  }
 
-
-        showScrollableMenu(scroll);
-        if(router.query.index){
-          AddCountry(router.query.index)
-          pays.current[0].classList.add('ProjectsLayout_menu__link--selected__Is402')
-        }else{
-          AddCountry('EUROPE')
-
-        }
-
-
-      },[router.query.index])
-
-     function ChangePays(e){
-        if(!pays.current.includes(e)){
-          pays.current.push(e)
-         pays.current = pays.current.filter(e => e !== null)
-        }
- 
-
-     }
-      
-
-    return ( 
-
-        <div className={styles["menu-container"]}>
-        <ul className={styles["menu"]} id={styles["scrollable"]}  ref={scrollable}>
-          <li onClick={(e)=>{
-            select(e)
-            MenuSelected(e)
-          }} ref={ChangePays}
-            className={`${styles["menu__link"]} ${styles["menu__link--selected"]} `}
-          >
-            Top causes
-          </li>
-          {tabcountry.map(country =>{
-            return (
-              <li onClick={(e)=>{
-                select(e)
-                MenuSelected(e)
-
-              }} ref={ChangePays} key={country} className={styles["menu__link"]}>{country}</li>
-
-            )
-          })}
- 
-
-        </ul>
-        <div className={styles["menu-container__icon"]}>
-          {isShown ? scrollButton : null}
-        </div>
+  return (
+    <div className={styles["menu-container"]}>
+      <ul className={styles["menu"]} id={styles["scrollable"]} ref={scrollable}>
+        <li
+          onClick={(e) => {
+            select(e);
+            MenuSelected(e);
+          }}
+          ref={ChangePays}
+          className={`${styles["menu__link"]} ${styles["menu__link--selected"]} `}
+        >
+          Top causes
+        </li>
+        {tabcountry.map((country) => {
+          return (
+            <li
+              onClick={(e) => {
+                select(e);
+                MenuSelected(e);
+              }}
+              ref={ChangePays}
+              key={country}
+              className={styles["menu__link"]}
+            >
+              {country}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles["menu-container__icon"]}>
+        {isShown ? scrollButton : null}
       </div>
-     );
+    </div>
+  );
 }
 
 export default MenuCountry;
