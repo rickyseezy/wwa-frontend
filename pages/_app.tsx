bac51eec1f94776d001f8babd811fae25253710b
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-04-13 10:13:24 +0200

    setup maj plus environnementale variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,41 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
-import {closeContext} from '../context'
+import { closeContext } from "../context";
 import { useEffect, useState } from "react";
+import {fireApp} from '../firebase/configApp'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  let [close,setclose] = useState(false)
-  console.log(close)
+
+console.log(fireApp,'firebase')
+  let [close, setclose] = useState(false);
+  console.log(close);
   useEffect(() => {
-    console.log('effect')
+    console.log("effect");
 
-    const handleTabClose = event => {
+    const handleTabClose = (event: { preventDefault: () => void; returnValue: string; }) => {
       event.preventDefault();
-            // event.defaultPrevented()
+      // event.defaultPrevented()
 
-      console.log('beforeunload event triggered');
-      setclose(true)
+      console.log("beforeunload event triggered");
+      setclose(true);
 
-      return (event.returnValue =
-        '.....');
+      return (event.returnValue = "are you sure you want to leave ? ");
     };
 
-    window.addEventListener('beforeunload', handleTabClose);
+    window.addEventListener("beforeunload", handleTabClose);
 
     return () => {
-      window.removeEventListener('beforeunload', handleTabClose);
+      window.removeEventListener("beforeunload", handleTabClose);
     };
   }, []);
 
-
   return (
     <>
-    <closeContext.Provider value={close}>
-  <Component {...pageProps} />
-
-    </closeContext.Provider>
-  </>
-  )
+      <closeContext.Provider value={close}>
+        <Component {...pageProps} />
+      </closeContext.Provider>
+    </>
+  );
 }
 
 export default MyApp;
