f74fab5b89b069f456b034d05a47cd28e22ae7fb
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-04-14 15:55:10 +0200

    config

--- firebase/configApp.js
+++ firebase/configApp.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 
+console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
