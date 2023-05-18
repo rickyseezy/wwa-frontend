ad5857075509c86e25d5a47a2928384645e0b9f8
Soma Bini <binisoma@gmail.com>
2023-05-18 20:27:46 +0400

    Add sign in

--- domain/repositories/authenticator.ts
+++ domain/repositories/authenticator.ts
@@ -1,9 +1,9 @@
-import {createUserWithEmailAndPassword, getAuth} from "@firebase/auth";
-import firebase from "firebase/compat";
-import auth = firebase.auth;
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "@firebase/auth";
+
 
 interface IAuthenticatorRepository {
     CreateUser(email: string, password: string): Promise<string>
+    Login(email: string, password: string): Promise<string>
 }
 
 export default class AuthenticatorRepository implements IAuthenticatorRepository {
@@ -15,6 +15,15 @@ export default class AuthenticatorRepository implements IAuthenticatorRepository
         } catch (e) {
             throw e
         }
+    }
 
+    async Login(email, password): Promise<string> {
+        const auth = getAuth();
+       try {
+           const data = await signInWithEmailAndPassword(auth, email, password)
+           return data?.user?.uid
+       }catch (e) {
+           throw e
+       }
     }
 }
