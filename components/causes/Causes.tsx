18f00b681694beb5bd76b6aa83458f0d6b4ba0f0
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-10-23 15:03:56 +0200

    update causes

--- components/causes/Causes.tsx
+++ components/causes/Causes.tsx
@@ -1,56 +1,14 @@
 import styles from "./Causes.module.scss";
 import Card from "@components/card/Card";
-
+let tabCause = new Array(10).fill(0)
 const Causes = () => {
   return (
     <ul className={styles["country-causes"]}>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-      <li>
-        <Card />
-      </li>
-    </ul>
-  );
+      {
+        tabCause.map(el => <Card content={undefined} />)
+      }
+  </ul>
+  )
 };
 
 export default Causes;
